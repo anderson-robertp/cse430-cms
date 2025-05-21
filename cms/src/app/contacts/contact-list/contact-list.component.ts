733e2af32c6e720aa21e76247ea5283dd0f20e2f
anderson-robertp <anderon-rp@hotmail.com>
2025-05-20 20:38:25 -0500

    contact service created

--- cms/src/app/contacts/contact-list/contact-list.component.ts
+++ cms/src/app/contacts/contact-list/contact-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 
 import { Contact } from '../contact.model';
+import { ContactsService } from '../contacts.service';
+
 
 @Component({
   selector: 'cms-contact-list',
@@ -11,29 +13,16 @@ import { Contact } from '../contact.model';
 export class ContactListComponent {
   @Output() contactWasSelected = new EventEmitter<Contact>();
   
-  contacts: Contact[] = [
-    new Contact(
-      "1",
-      "R. Kent Jackson",
-      "jacksonk@byui.edu",
-      "208-496-3771",
-      "../../../assets/images/jacksonk.jpg",
-      null
-    ),
-    new Contact(
-      "2",
-      "Rex Barzee",
-      "barzeer@byui.edu",
-      "208-496-3768",
-      "../../../assets/images/barzeer.jpg",
-      null
-    )
-  ];
+  contacts: Contact[] = [];
 
   onSelected(contact: Contact) {
     console.log('Contact emitted from ContactListComponent:', contact);
     this.contactWasSelected.emit(contact);
   }
 
-  constructor() {}
+  constructor(private contactServe: ContactsService) {}
+
+  ngOnInit() {
+    this.contacts = this.contactServe.getContacts();
+  }
 }
---
+++ cms/src/app/contacts/contacts.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Contact } from './contact.model';
+import { MOCKCONTACTS } from './MOCKCONTACTS';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactsService {
+  contacts: Contact[] = [];
+
+  constructor() {
+    this.contacts = MOCKCONTACTS;
+   }
+
+  getContacts() {
+    return this.contacts.slice();
+  }
+
+  getContact(id: string) {
+    for (let contact of this.contacts) {
+      if (contact.id === id) {
+        return contact;
+      } else {
+        return null;
+      }
+    } 
+  }
+}
